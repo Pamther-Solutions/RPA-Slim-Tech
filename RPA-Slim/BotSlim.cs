01bb6c33591e1518a0136cbb372af9cd240aea40
Gabriel <ga.lima2205@gmail.com>
2024-01-23 22:22:39 -0300

    update code

---
+++ RPA-Slim/BotSlim.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPA_Slim
+{
+    public class BotSlim
+    {
+        IWebDriver driver;
+
+
+
+        public BotSlim()
+        {
+            driver = new ChromeDriver();
+        }
+
+
+        public void Bot()
+        {
+            driver.Navigate().GoToUrl("https://slim-tech.netlify.app/");
+
+            driver.FindElement(By.XPath("//*[@id=\"contato\"]/div[1]/form/div/div[1]/input[1]")).SendKeys("oi");
+            driver.FindElement(By.XPath("//*[@id=\"contato\"]/div[1]/form/div/div[1]/input[2]")).SendKeys("oi");
+            driver.FindElement(By.XPath("//*[@id=\"contato\"]/div[1]/form/div/div[2]/input[1]")).SendKeys("oi");
+            driver.FindElement(By.XPath("//*[@id=\"contato\"]/div[1]/form/div/div[2]/input[2]")).SendKeys("oi");
+            driver.FindElement(By.XPath("//*[@id=\"contato\"]/div[1]/form/div/textarea")).SendKeys("oi");
+
+        }
+
+        
+    }
+}
--- RPA-Slim/Program.cs
+++ RPA-Slim/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RPA_Slim;
+
+BotSlim botSlim = new BotSlim();
+
+botSlim.Bot();
